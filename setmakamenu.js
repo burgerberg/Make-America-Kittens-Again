52687a1380040e41e647b5ecb13bb352b5693b21
burgerberg <hegenb@yahoo.com.br>
2017-07-31 14:06:18 -0300

    Update setmakamenu.js

--- setmakamenu.js
+++ setmakamenu.js
@@ -1,9 +1,4 @@
-// setmakamenu.js - part of make america kittens again
-// v1.1.3
-// by Tom Royal 
-// tomroyal.com
-
-// sets up a menu item to invoke the de-kittening
+// MENU PARA DESFAZER
 
 function DeMakaMenuClick(info, tab) {
     chrome.tabs.query({
@@ -11,22 +6,9 @@ function DeMakaMenuClick(info, tab) {
         "currentWindow": true
     }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
-            "functiontoInvoke": "undoMAKA"
+            "functiontoInvoke": "undoDORIA"
         });
     });
 }
-/*
-function ReMakaMenuClick(info, tab) {
-    chrome.tabs.query({
-        "active": true,
-        "currentWindow": true
-    }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            "functiontoInvoke": "redoMAKA"
-        });
-    });
-}
-*/
 
-var setMAKAmenu1 = chrome.contextMenus.create({"title": "Make Kittens Trump Again :(", "contexts":["page"], "onclick": DeMakaMenuClick});
-// var setMAKAmenu2 = chrome.contextMenus.create({"title": "MORE KITTENS", "contexts":["page"], "onclick": ReMakaMenuClick});
+var setMAKAmenu1 = chrome.contextMenus.create({"title": "Trazer o Doriana de volta :(", "contexts":["page"], "onclick": DeMakaMenuClick});
